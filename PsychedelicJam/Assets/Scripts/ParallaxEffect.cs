08f818caf077e3b3e97653f3c3c7f5e537a12826
gmeisinger <gmeisinger@gmail.com>
2021-09-19 22:12:24 -0500

    added parallax background, main menu placeholder

---
+++ PsychedelicJam/Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private float length, startpos;
+
+    public GameObject cam;
+    public float parallaxFactor;
+
+    private void Awake()
+    {
+        if (cam == null)
+            cam = Camera.main.gameObject;
+    }
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float temp = cam.transform.position.x * (1 - parallaxFactor);
+        float distance = cam.transform.position.x * parallaxFactor;
+        Vector3 newPosition = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        transform.position = newPosition;
+
+        if (temp > startpos + (length / 2)) startpos += length;
+        else if (temp < startpos - (length / 2)) startpos -= length;
+    }
+}
--- PsychedelicJam/Assets/Scripts/Player/PlayerCamera.cs
+++ PsychedelicJam/Assets/Scripts/Player/PlayerCamera.cs
@@ -6,25 +6,16 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class PlayerCamera : MonoBehaviour
 {
     public Transform playerTransform;
+    //public SpriteRenderer background;
     [Range(0, .5f)] public float smoothTime = 0;
-    //[Range(0, 180)] public int zoom = 0;
 
     private Vector3 velocity;
-    //private PixelPerfectCamera ppCam;
-    //private int defaultResolutionX;
-    //private int defaultResolutionY;
-
-    //private void Awake()
-    //{
-    //    ppCam = GetComponent<PixelPerfectCamera>();
-    //    defaultResolutionX = ppCam.refResolutionX;
-    //    defaultResolutionY = ppCam.refResolutionY;
-    //}
+    // I'll be honest, I got this value through trial and error and I'm ashamed.
+    private float bounds = 45;
 
     private void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z), ref velocity, smoothTime);
-        //ppCam.refResolutionX = defaultResolutionX - zoom * 2;
-        //ppCam.refResolutionY = defaultResolutionY - zoom;
+        Vector3 target = new Vector3(playerTransform.position.x, Mathf.Clamp(playerTransform.position.y, -bounds, bounds), transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
     }
 }
